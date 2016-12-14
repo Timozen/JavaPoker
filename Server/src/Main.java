ea6c88833f663e14d1ec61a3934b4933000c57ab
tim.buechner <tim.buechner@uni-jena.de>
2016-12-14 15:04:51 +0100

    Removed unnecessary imports

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -14,15 +14,6 @@
  */
 
 import connection.server.ConnectionServer;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main {
 	
