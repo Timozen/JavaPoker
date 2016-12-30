c275c1ddf767b52291f5fb883328394f3cd78615
DerInterneter <matthias@doepmann.org>
2016-12-30 19:26:42 +0100

    Implemented 3rd Party class "Message Console" (see license for source)

---
+++ Client/src/javapoker/client/messageconsole/LimitLinesDocumentListener.java
@@ -0,0 +1,7 @@
+package javapoker.client.messageconsole;
+
+/**
+ * Created by Derpie on 30.12.2016.
+ */
+public class LimitLinesDocumentListener {
+}
---
+++ Client/src/javapoker/client/messageconsole/MessageConsole.java
@@ -0,0 +1,7 @@
+package javapoker.client.tips4java;
+
+/**
+ * Created by Derpie on 30.12.2016.
+ */
+public class MessageConsole {
+}
